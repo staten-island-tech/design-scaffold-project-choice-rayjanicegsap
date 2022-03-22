8497c975135e130d8d9206f5938f540d6632d5db
JaniceChen11 <janicechen910@yahoo.com>
2022-03-22 13:12:52 -0400

    p

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,2 +1,24 @@
-
 import "../css/style.css";
+
+const attractions = [
+    {
+    name: `The Eiffle Tower`,
+    description: `A beautiful tower constructed out of iron beams. The heart of Paris. Next to the tower rests Champs de Mars and a magnificent bridge called the Pont Alexandre III.`,
+    sale: "yes",
+    imageurl: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDN8fHBhcmlzfGVufDB8fHx8MTY0Nzk2OTExMg&ixlib=rb-1.2.1&q=85&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450"
+  }
+]
+let cards = attractions;
+function insertCards() {
+    cards.forEach((attractions) => {
+        document.querySelector(".display").insertAdjacentHTML(
+            "beforeend",
+            `<div class="display-card">
+            <img class="image" src="${attractions.imageurl}" />
+            <h2 class="name">${attractions.name}</h2>
+            <h2 class="description">${attractions.description}</h2>
+          </section>`
+          );
+    });
+}
+insertCards();
