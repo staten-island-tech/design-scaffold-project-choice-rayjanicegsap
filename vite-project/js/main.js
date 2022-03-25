6bd43dd3c3bed7922f2371559a220b74e7ecc64e
JaniceChen11 <janicechen910@yahoo.com>
2022-03-25 13:06:22 -0400

    Edits

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,36 +1,6 @@
 import "../css/style.css";
 
-const DOMselectors = {
-  Attractions: document.getElementById("Attractions"),
-  Food: document.getElementById("Food"),
-  Tours: document.getElementById("Tours"),
-  Customs: document.getElementById("Customs"),
-  Aboutus: document.getElementById("Aboutus"),
-};
-const Buttonselectors = {
-  btn1: document.getElementById("btn1"),
-  btn2: document.getElementById("btn2"),
-  btn3: document.getElementById("btn3"),
-  btn4: document.getElementById("btn4"),
-  btn5: document.getElementById("btn5"),
-};
-Buttonselectors.btn1.addEventListener("click", function () {
-  DOMselectors.Attractions.scrollIntoView();
-  });
-Buttonselectors.btn2.addEventListener("click", function () {
-  DOMselectors.Food.scrollIntoView();
-  });
-Buttonselectors.btn3.addEventListener("click", function () {
-  DOMselectors.Tours.scrollIntoView();
-  });
-  Buttonselectors.btn4.addEventListener("click", function () {
-    DOMselectors.Customs.scrollIntoView();
-    });
-    Buttonselectors.btn5.addEventListener("click", function () {
-      DOMselectors.Aboutus.scrollIntoView();
-      });
-
-/* const attractions = [
+ const attractions = [
     {
     name: `The Eiffle Tower`,
     description: `A beautiful tower constructed out of iron beams. The heart of Paris. Next to the tower rests Champs de Mars and a magnificent bridge called the Pont Alexandre III.`,
@@ -52,4 +22,4 @@ Buttonselectors.btn3.addEventListener("click", function () {
      });
  }
  insertCards();
- */
+ 
