97e878afc9c221843856ce7707ea5e86e05f25b1
Ray Zhu <ray6463393172@gmail.com>
2022-03-25 10:56:53 -0400

    fefe
    feef

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,2 +1,13 @@
-
 import "../css/style.css";
+const DOMselectors = {
+  Attractions: document.getElementById("Attractions"),
+  Food: document.getElementById("Food"),
+  Tours: document.getElementById("Tours"),
+  Customs: document.getElementById("Customs"),
+  Aboutus: document.getElementById("Aboutus"),
+};
+DOMselectors.array.forEach((element) => {
+  document.addEventListener("click", function () {
+    window.scrollTo;
+  });
+});
