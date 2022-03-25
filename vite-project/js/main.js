07e2cd77885ba982ece3da13e861271010eb180e
RayZhu1004 <90782207+RayZhu1004@users.noreply.github.com>
2022-03-25 11:12:48 -0400

    fdvdw
    efwewf

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -6,8 +6,25 @@ const DOMselectors = {
   Customs: document.getElementById("Customs"),
   Aboutus: document.getElementById("Aboutus"),
 };
-DOMselectors.array.forEach((element) => {
-  document.addEventListener("click", function () {
-    window.scrollTo;
+const Buttonselectors = {
+  btn1: document.getElementById("btn1"),
+  btn2: document.getElementById("btn2"),
+  btn3: document.getElementById("btn3"),
+  btn4: document.getElementById("btn4"),
+  btn5: document.getElementById("btn5"),
+};
+Buttonselectors.btn1.addEventListener("click", function () {
+  DOMselectors.Attractions.scrollIntoView();
   });
-});
+Buttonselectors.btn2.addEventListener("click", function () {
+  DOMselectors.Food.scrollIntoView();
+  });
+Buttonselectors.btn3.addEventListener("click", function () {
+  DOMselectors.Tours.scrollIntoView();
+  });
+  Buttonselectors.btn4.addEventListener("click", function () {
+    DOMselectors.Customs.scrollIntoView();
+    });
+    Buttonselectors.btn5.addEventListener("click", function () {
+      DOMselectors.Aboutus.scrollIntoView();
+      });
