45bc553f6e933d1a4ac4f577cafd94e501d97454
JaniceChen11 <janicechen910@yahoo.com>
2022-03-25 12:56:30 -0400

    Edits

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,35 @@
 import "../css/style.css";
 
+const DOMselectors = {
+  Attractions: document.getElementById("Attractions"),
+  Food: document.getElementById("Food"),
+  Tours: document.getElementById("Tours"),
+  Customs: document.getElementById("Customs"),
+  Aboutus: document.getElementById("Aboutus"),
+};
+const Buttonselectors = {
+  btn1: document.getElementById("btn1"),
+  btn2: document.getElementById("btn2"),
+  btn3: document.getElementById("btn3"),
+  btn4: document.getElementById("btn4"),
+  btn5: document.getElementById("btn5"),
+};
+Buttonselectors.btn1.addEventListener("click", function () {
+  DOMselectors.Attractions.scrollIntoView();
+  });
+Buttonselectors.btn2.addEventListener("click", function () {
+  DOMselectors.Food.scrollIntoView();
+  });
+Buttonselectors.btn3.addEventListener("click", function () {
+  DOMselectors.Tours.scrollIntoView();
+  });
+  Buttonselectors.btn4.addEventListener("click", function () {
+    DOMselectors.Customs.scrollIntoView();
+    });
+    Buttonselectors.btn5.addEventListener("click", function () {
+      DOMselectors.Aboutus.scrollIntoView();
+      });
+
 const attractions = [
     {
     name: `The Eiffle Tower`,
@@ -17,7 +47,7 @@ const attractions = [
              <img class="image" src="${attractions.imageurl}" />
              <h3 class="name">${attractions.name}</h3>
              <h4 class="description">${attractions.description}</h4>
-           </section>`
+           </div>`
            );
      });
  }
